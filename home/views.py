216bb51dffc87c12c95210b4957d9dfdf5be1f76
AvinashKhanduri <Khanduria11@gmail.com>
2024-11-13 07:58:45 +0530

    data loded form backend

--- home/views.py
+++ home/views.py
@@ -7,7 +7,6 @@ def landingPage(request):
     return render(request, 'home/landingPage.html')
 
 def homepage(request):
-    movieData = Movie.objects.all()
-    
-
-    return render(request,'home/home.html')
+    movieData = Movie.objects.all()[:10]
+    recommended = Movie.objects.all()[10:20]
+    return render(request,'home/home.html',context={"movieData":movieData,"recommended":recommended})
